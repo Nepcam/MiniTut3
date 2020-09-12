abc6cb3fa719a481f20c373f899978823be52eb4
Cameron NEPE <cn93@students.waikato.ac.nz>
2020-09-12 22:28:29 +1200

    Add project files.

---
+++ MiniTut3/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MiniTut3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxData = new System.Windows.Forms.ListBox();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxData
+            // 
+            this.listBoxData.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxData.FormattingEnabled = true;
+            this.listBoxData.ItemHeight = 23;
+            this.listBoxData.Location = new System.Drawing.Point(21, 31);
+            this.listBoxData.Name = "listBoxData";
+            this.listBoxData.Size = new System.Drawing.Size(928, 487);
+            this.listBoxData.TabIndex = 0;
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTest.Location = new System.Drawing.Point(362, 542);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(146, 51);
+            this.buttonTest.TabIndex = 1;
+            this.buttonTest.Text = "Test";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 620);
+            this.Controls.Add(this.buttonTest);
+            this.Controls.Add(this.listBoxData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxData;
+        private System.Windows.Forms.Button buttonTest;
+    }
+}
+
